2a2d33f59721034af2d05b132063082c3f07959b
IfThingsThenStuff <ifthingsthenstuff@gmail.com>
2024-01-10 17:02:12 -0500

    Add swagger support

--- src/OAuth1-Sample.Server/Program.cs
+++ src/OAuth1-Sample.Server/Program.cs
@@ -1,3 +1,4 @@
+
 namespace OAuth1_Sample.Server;
 
 public class Program
@@ -9,10 +10,18 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
         app.UseHttpsRedirection();
 
